a9d702329af564a00ddbb2038a48685c7ff4a4d6
Việt Nguyễn <kido@Viets-MacBook-Pro.local>
2022-04-01 11:43:49 +0700

    Internationalization web app multiple language

---
+++ src/test/java/kido/internationalization/InternationalizationApplicationTests.java
@@ -0,0 +1,13 @@
+package kido.internationalization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternationalizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
