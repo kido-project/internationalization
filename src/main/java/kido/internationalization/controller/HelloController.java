595d994f02033f79e4c3fabb07f1247a96ccce3c
Việt Nguyễn <kido@Viets-MacBook-Pro.local>
2022-04-04 22:49:41 +0700

    Create theme leaf with boostrap demo

---
+++ src/main/java/kido/internationalization/controller/HelloController.java
@@ -0,0 +1,13 @@
+package kido.internationalization.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+        return "hello";
+    }
+}
